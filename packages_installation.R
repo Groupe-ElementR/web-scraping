df14fe0ccdcc6a9c9c7a8dbdeacc3462fa24c1fa
Robin Cura <robin.cura@gmail.com>
2016-03-30 13:24:18 +0200

    add packages installation script

---
+++ packages_installation.R
@@ -0,0 +1,5 @@
+install.packages(c("dplyr", "knitr",  "magrittr", "rmdformats", "sp", "mapview", "geonames", "xml2", "rvest", "stringr", "tidyr", "ggplot2", "tm", "tm.plugin.webmining", "wordcloud", "RColorBrewer"))
+
+devtools::install_github(repo = 'rCarto/photon')
+
+devtools::install_github("rCarto/osrm")
